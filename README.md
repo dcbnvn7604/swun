06e7623f91f51e9edb1f39018879e7730daf297c
Falgor.C <tranhoa.7604@gmail.com>
2025-02-04 10:24:17 +0700

    timer.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # swun
 1. cpu and ram usage monitoring to identify the cause of system hang-ups.
+2. timer in browser
---
+++ timer.js
@@ -0,0 +1,56 @@
+class Timer {
+    timer = (minutes=10) => {
+        this.startAt = Date.now();
+        if (Notification.permission !== "granted") {
+            throw 'No permission';
+        }
+        console.log(`timer start at:${new Date()}`);
+        this.timeout = setTimeout(this.finish, minutes * 60 * 1000);
+    }
+
+    stopT  = () => {
+        clearTimeout(this.timeout);
+        this.startAt = null;
+    }
+
+    spendT = () => {
+        if (!this.startAt) {
+            throw 'Not start';
+        }
+        let spend = (Date.now() - this.startAt) / 1000 / 60;
+        console.log(`spend: ${spend.toFixed(1)} minutes`);
+    }
+
+    finish = () => {
+        this.startAt = null;
+        new Notification("Timeout");
+    }
+
+    requestPermission = () => {
+        let button = document.createElement("button");
+        button.innerHTML = "Request permission";
+        button.addEventListener('click', () => {Notification.requestPermission()});
+        document.body.appendChild(button);
+    }
+
+    alarm = (at) => {
+        if (!at) {
+            throw 'Alarm: No time';
+        }
+        let parts = at.match(/(\d{2}):(\d{2})/);
+        if (!parts) {
+            throw 'Alarm: Wrong time';
+        }
+        let alarmAt = new Date();
+        alarmAt.setHours(parts[1], parts[2]);
+        this.alarmer = setTimeout(this.finish, alarmAt.getTime() - Date.now());
+        console.log(`alarm at:${alarmAt}`);
+    }
+
+    stopA  = () => {
+        clearTimeout(this.alarmer);
+    }
+};
+let _t = new Timer();
+window.t=_t.timer;
+window.a=_t.alarm;
