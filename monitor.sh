e2c6eb093f34b1091ac76a618b254eda6d0bf9d2
Falgor.C <tranhoa.7604@gmail.com>
2025-02-07 09:56:48 +0700

    [monitor.sh] different details

--- monitor.sh
+++ monitor.sh
@@ -13,27 +13,43 @@ extract_data() {
     }'
 }
 
-save_top_output() {
+output_head() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") warn_lagging freecpu($2) freemem($3)" >> $output
     echo "$1" >> $output
 }
 
+output_tail_cpu() {
+    detail_top_output="$(top -b -n1 -Em -o +%CPU | tail -n +6)"
+    echo "$detail_top_output" >> $output
+}
+
+output_tail_mem() {
+    detail_top_output="$(top -b -n1 -Em -o +%MEM | tail -n +6)"
+    echo "$detail_top_output" >> $output
+}
+
+
 while true; do
-    top_output="$(top -b -n1 -Em)"
+    top_output="$(top -b -n1 -Em | head -n5)"
     read cpu_idle free_mem < <(extract_data "$top_output")
     cpu_idle="${cpu_idle//,/.}"
     free_mem="${free_mem//,/.}"
     cpu_idle_check=$(echo "$cpu_idle < $max" | bc -l 2>&1)
     free_mem_check=$(echo "$free_mem < $max" | bc -l 2>&1)
     if [[ "$cpu_idle_check" == "1" || "$free_mem_check" == "1" ]]; then
-        save_top_output "$top_output" "$cpu_idle" "$free_mem"
+        output_head "$top_output" "$cpu_idle" "$free_mem"
+        if [["$cpu_idle_check" == "1"]]; then
+            output_tail_cpu
+        else
+            output_tail_mem
+        fi
         period=1
     else
         period=5
     fi
     
     if [[ ("$cpu_idle_check" != "1" && "$cpu_idle_check" != "0" && "$cpu_idle" != "id." ) || ("$free_mem_check" != "1" && "$free_mem_check" != "0") ]]; then
-        echo "debug_syntax freecpu($cpu_idle) freemem($free_mem) $cpu_idle_check $free_mem_check" >> $output
+        echo "debug freecpu($cpu_idle) freemem($free_mem) $cpu_idle_check $free_mem_check" >> $output
         echo "$top_output" >> $output
     fi
     
